843201a67b6287739f061e205a01e99393701a05
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2023-09-09 17:57:33 +0300

    !

--- js/task-10.js
+++ js/task-10.js
@@ -1,36 +1,37 @@
-const createBtnRef = document.querySelector("button[data-create]");
-const destroyBtnRef = document.querySelector("button[data-destroy]");
-const boxRef = document.querySelector("div#boxes");
+// const createBtnRef = document.querySelector("button[data-create]");
+// const destroyBtnRef = document.querySelector("button[data-destroy]");
+// const boxRef = document.querySelector("div#boxes");
 
-createBtnRef.addEventListener("click", createBoxesOnClick);
-destroyBtnRef.addEventListener("click", destroyBoxesOnClick);
+// createBtnRef.addEventListener("click", createBoxesOnClick);
+// destroyBtnRef.addEventListener("click", destroyBoxesOnClick);
 
-let step = 30;
-function createBoxesOnClick() {
-  const amount = document.querySelector('input[type="number"]').value;
-  // const arrBoxes = [];
+// let step = 30;
+// function createBoxesOnClick() {
+//   const amount = document.querySelector('input[type="number"]').value;
+//   const arrBoxes = [];
 
-  for (let i = 0; i < amount; i += 1) {
-    step += 10;
-    const newBox = document.createElement("div");
-    newBox.style.width = `${step}px`;
-    newBox.style.height = `${step}px`;
-    newBox.style.backgroundColor = getRandomHexColor();
-    boxRef.appendChild(newBox);
-    //! чи можна створювати розмітку в циклі ?
-    // arrBoxes.push(newBox);
-  }
-  // boxRef.append(...arrBoxes);
-  //! це другий варіант - прибрати 19 рядок и залишити 11,21,23.
-}
+//   for (let i = 0; i < amount; i += 1) {
+//     step += 10;
+//     const newBox = document.createElement("div");
+//     newBox.style.width = `${step}px`;
+//     newBox.style.height = `${step}px`;
+//     newBox.style.backgroundColor = getRandomHexColor();
+//     // boxRef.appendChild(newBox);
+//     //! чи можна створювати розмітку в циклі ?
+//     arrBoxes.push(newBox);
+//   }
+//   boxRef.append(...arrBoxes);
+//   //! це другий варіант - прибрати 19 рядок и залишити 11,21,23.
+// }
 
-function destroyBoxesOnClick() {
-  boxRef.innerHTML = "";
-  step = 30;
-}
+// function destroyBoxesOnClick() {
+//   boxRef.innerHTML = "";
+//   step = 30;
+// }
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
